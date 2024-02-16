74a36fe1155103da17398819783637aaab1a273a
strelok93 <strelok0493@gmail.com>
2024-02-16 16:28:11 +0500

    f

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="http://wl.ngmk.uz/wsdk/script/wialon.js"></script>
-    <script src="/map/leaflet.js"></script>
-    <link rel="stylesheet" href="/map/leaflet.css" />
+    <script src="/map/leaflet.js?v=1"></script>
+    <link rel="stylesheet" href="/map/leaflet.css?v=1" />
     <link rel="stylesheet" href="/awesome/css/all.min.css" />
     <title>Карта Скоростных дорог</title>
 </head>
