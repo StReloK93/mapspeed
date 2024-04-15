f0459799da9276bb5cb47cd1f3f9118c1fd3450a
strelok93 <strelok0493@gmail.com>
2024-04-15 10:02:15 +0500

    fa

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -7,7 +7,7 @@ import 'v-calendar/style.css'
 import { createPinia } from 'pinia'
 
 
-const token = "94e3f3e1ac97def632645f3655f7c932495467B36E348AF15584E7D4D3B7320025939C25"
+const token = "94e3f3e1ac97def632645f3655f7c932F353B5B91B9E1B0EA7B79A39D6672B70685972C0"
 wialon.core.Session.getInstance().initSession("http://wl.ngmk.uz")
 wialon.core.Session.getInstance().loginToken(token, "", () => {
     createApp(App)
