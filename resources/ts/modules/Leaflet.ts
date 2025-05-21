5513fd29ef4562c3c3135d4d21a8148b6b3c708b
strelok93 <strelok0493@gmail.com>
2025-05-21 16:44:20 +0500

    as

--- resources/ts/modules/Leaflet.ts
+++ resources/ts/modules/Leaflet.ts
@@ -79,7 +79,7 @@ export default class {
         // var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
         L.TileLayer.WebGis = L.TileLayer.extend({
             initialize: function (url, options) {
-                this._url = `http://wl.ngmk.uz/gis_render/{x}_{y}_{z}/${options.userId}/tile.png?sid=${sid}`
+                this._url = `http://172.16.0.166:8025/gis_render/{x}_{y}_{z}/${options.userId}/tile.png?sid=${sid}`
             },
             getTileUrl: getTile
         })
